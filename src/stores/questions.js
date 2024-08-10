034df6d189997a13ee1cb20da00e989b4a5603b7
eduruiz <contato@eduruiz.com.br>
2024-08-10 03:25:35 -0300

    Rewrite questions store in composition style so we can use watch for i18n

--- src/stores/questions.js
+++ src/stores/questions.js
@@ -1,41 +1,65 @@
 import { defineStore } from 'pinia'
-import quiz from "../data/teste.json"
-import sections from "../data/sections.json"
+import { ref, computed, watch } from 'vue'
+import { useI18n } from 'vue-i18n';
+import { i18n } from '@/i18n';
+import quizPtBR from '@/data/ptBR/quiz.json';
+import quizEn from '@/data/en/quiz.json';
+import sectionsPtBR from '@/data/ptBR/sections.json';
+import sectionsEn from '@/data/en/sections.json';
 
-export const useQuestionsStore = defineStore('questions', {
-  state: () => {
-    return {
-      questions: [quiz.questions['1']],
+export const useQuestionsStore = defineStore('questions', () => {
+  const currentQuestion = computed(() => questions.value[questions.value.length - 1]);
+  const currentSection = computed(() => {
+    return sections.value.find(section => section.id_section === currentQuestion.value.section);
+  });
+  const { locale } = useI18n();
+  let quiz = quizPtBR;
+  let questions = ref([quiz.questions['1']]);
+  let sections = ref(sectionsPtBR);
+
+  async function selectLanguage() {
+    const language = i18n.global.locale.value;
+    quiz = language === 'en' ? quizEn : quizPtBR;
+    sections = language === 'en' ? sectionsEn : sectionsPtBR;
+  }
+
+  function resetState() {
+    questions.value = [quiz.questions['1']];
+  };
+
+  function addQuestion(questionId) {
+    questions.value.push(quiz.questions[questionId]);
+  };
+
+  async function chooseOption(option, question) {
+    if (option.end) {
+      const sectionId = question.section;
+      // in case we end up without next_question_id;
+      // const nextSectionId = sections.indexOf(currentSection) + 1;
+      // const nextSection = sections[nextSectionId];
+      // option.next_question_id = nextSection.id_question;
+
+      await this.router.push({ name: 'milestone', params: { name: currentSection.value.name } })
     }
-  },
-  persist: true,
-  actions: {
-    resetState() {
-      this.questions = [quiz.questions['1']];
-    },
-    addQuestion(questionId) {
-      this.questions.push(quiz.questions[questionId]);
-    },
-    async chooseOption(option, question) {
-      if (option.end) {
-        const sectionId = question.section;
-        const currentSection = sections.find((section) => section.id_section === sectionId);
-        // in case we end up without next_question_id;
-        // const nextSectionId = sections.indexOf(currentSection) + 1;
-        // const nextSection = sections[nextSectionId];
-        // option.next_question_id = nextSection.id_question;
 
-        await this.router.push({ name: 'milestone', params: { name: currentSection.name } })
-      }
+    if (option.final) {
+      await this.router.push({ name: 'congratulations' })
+    }
 
-      if (option.final) {
-        await this.router.push({ name: 'congratulations' })
-      }
+    addQuestion(option.next_question_id);
+  };
 
-      this.addQuestion(option.next_question_id);
-    },
-  },
-  getters: {
-    currentQuestion: (state) => state.questions[state.questions.length - 1],
-  },
-})
+  watch(locale, () => {
+    selectLanguage();
+  });
+
+  return {
+    questions,
+    currentQuestion,
+    resetState,
+    addQuestion,
+    chooseOption,
+    sections,
+    currentSection
+  }
+}, { persist: true });
