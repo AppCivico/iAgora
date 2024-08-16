6e4930cca40b4f7f014097e474ced0fc2a5c248c
Sobral <robson@oficiodesign.com>
2024-08-16 17:37:59 -0300

    build: Include sourcemaps on Sentry reports

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Markdown from 'unplugin-vue-markdown/vite'
 
 export default defineConfig({
-  plugins: [
-    vue({
-      include: [/\.vue$/, /\.md$/],
-    }),
-    Markdown({
-      linkify: true,
-      typographer: true
-    }),
-  ],
+  plugins: [vue({
+    include: [/\.vue$/, /\.md$/],
+  }), Markdown({
+    linkify: true,
+    typographer: true
+  }), sentryVitePlugin({
+    org: "appcivico",
+    project: "iagora"
+  })],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -22,5 +24,9 @@ export default defineConfig({
       '@images': fileURLToPath(new URL('./src/assets/images', import.meta.url)),
       '@fonts': fileURLToPath(new URL('./src/assets/fonts', import.meta.url)),
     }
+  },
+
+  build: {
+    sourcemap: true
   }
 });
